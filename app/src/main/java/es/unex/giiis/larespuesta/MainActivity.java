07dd528ad61e1bd35b4da3bd90530364d753e010
RobertoRE <rrecheve@gmail.com>
2022-09-19 16:34:33 +0200

    update to Gradle 7.4 and AGP 7.3.0

--- app/src/main/java/es/unex/giiis/larespuesta/MainActivity.java
+++ app/src/main/java/es/unex/giiis/larespuesta/MainActivity.java
@@ -3,12 +3,15 @@ package es.unex.giiis.larespuesta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final int[] answers = { 42, -10, 0, 100, 1000 };
     public static final int answer = 42;
+
+    private static final String TAG = "MyActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView answerView = (TextView) findViewById(R.id.respuesta_view);
 
-        //TODO encuentra el bug y solucionalo
         int val = findAnswer();
+        Log.d(TAG,"El valor devuelto por la funcion es: "+val);
         String output = (val == answer) ? "42" : "Puede que nunca la sepamos";
         answerView.setText("La respuesta a la vida, al universo a todo es:\n\n"
                 + output);
@@ -26,7 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int findAnswer() {
         for (int val : answers) {
-            if (val != answer)
+            Log.d(TAG,"El valor dentro del bucle es: "+val);
+            if (val == answer)
                 return val;
         }
         return -1;
